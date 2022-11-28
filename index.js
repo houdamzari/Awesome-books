ec41e584b3351171e50bc3d3c6b3315324940d7f
houdamzari <houdamzari1999@gmail.com>
2022-11-28 11:23:30 +0100

    Set up localStorage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable linebreak-style */
 const form = document.querySelector('form');
 const addButton = document.querySelector('.AddButton');
@@ -18,10 +20,14 @@ let bookList = [
   },
 ];
 
+function removeBook(bookId) {
+  bookList = bookList.filter((book) => book.id !== bookId);
+}
+
 const updateBooks = () => {
   booksContainer.textContent = '';
   bookList.forEach((book) => {
-    let booksSection = `<div class='Book__section'>
+    const booksSection = `<div class='Book__section'>
   <div class='Book__section-item'>${book.BookTitle} (${book.id})</div>
   <div class='Book__section-item'>${book.BookAuthor}</div>
   <button class='remove-button' id="${book.id}">Remove</button>
@@ -32,21 +38,17 @@ const updateBooks = () => {
   removeButtons.forEach((button) =>
     button.addEventListener('click', (e) => {
       removeBook(e.target.id);
+      updateBooks();
     })
   );
 };
-
-function removeBook(bookId) {
-  bookList = bookList.filter((book) => book.id != bookId);
-  updateBooks();
-}
 
 updateBooks();
 
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
-  let Title = titleInput.value;
-  let Author = authorInput.value;
+  const Title = titleInput.value;
+  const Author = authorInput.value;
   const newBook = {
     id: Date.now(),
     BookTitle: Title,
@@ -56,3 +58,15 @@ addButton.addEventListener('click', (e) => {
   updateBooks();
   form.reset();
 });
+
+const onChange = (e) => {
+  e.preventDefault();
+  localStorage.setItem('title', titleInput.value);
+  localStorage.setItem('author', authorInput.value);
+};
+
+let titleStorage = localStorage.getItem('title');
+let authorStorage = localStorage.getItem('author');
+
+titleInput.addEventListener('change', onChange);
+authorInput.addEventListener('change', onChange);
